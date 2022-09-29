fba21ad27a9467cff9bc0e47d056746c6cfa6ab6
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-29 17:16:18 +0530

    first & final commit, completed the project.

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertBtn = document.getElementById("convert-btn")
+const inputEl = document.getElementById("input-el")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
+
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
+
+
+convertBtn.addEventListener("click", function(){
+   let baseValue = inputEl.value
+
+   lengthEl.textContent =
+        `
+        ${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feets |
+        ${baseValue} feets =  ${(baseValue / meterToFeet).toFixed(3)} meters
+
+        `
+
+   volumeEl.textContent =
+        `
+        ${baseValue} liters   = ${(baseValue * literToGallon).toFixed(3)} gallons |
+        ${baseValue} gallons  =  ${(baseValue / literToGallon).toFixed(3)}liters
+
+        `
+   massEl.textContent =
+        `
+        ${baseValue} kilos   = ${(baseValue * kiloToPound).toFixed(3)} pounds |
+        ${baseValue} pounds  = ${(baseValue / kiloToPound).toFixed(3)} kilos
+
+        `
+
+})
+
